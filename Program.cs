b7f39060169e58d63891ec005abc618fe5a08e0f
John Doe <zgvldmfm@gmail.com>
2022-12-26 12:36:01 +0300

    Lesson 2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+void check (int dayWeek) {
+  if (dayWeek == 6 || dayWeek == 7) {
+  Console.WriteLine("Вы выбрали выходной день");
+  }
+  else if (dayWeek < 1 || dayWeek > 7) {
+    Console.WriteLine("Вы не выбрали день недели");
+  }
+  else Console.WriteLine("Вы выбрали рабочий день");
+}
+
+check(dayWeek);
